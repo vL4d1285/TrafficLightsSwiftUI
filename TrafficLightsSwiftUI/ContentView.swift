51044a28812cc4baa6555ad777ee80ad8aba8eff
Vladislav Ryabtsev <vryabtsev@me.com>
2022-01-21 10:37:12 +0300

    done

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -7,15 +7,95 @@
 
 import SwiftUI
 
+enum Lights {
+    case allLightsIsOff
+    case red, yellow, green
+}
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    @State private var currentLight = Lights.allLightsIsOff
+    @State private var nameButton = "GO"
+    
+    fileprivate func nextColor() {
+        if currentLight == .red {
+            currentLight = .yellow
+        } else if currentLight == .yellow {
+            currentLight = .green
+        } else {
+            currentLight = .red
+        }
     }
+    
+    var body: some View {
+        
+        ZStack{
+            VStack{
+                if currentLight == .red {
+                    redLight.opacity(1)
+                    yellowLight.opacity(0.2)
+                    greenLight.opacity(0.2)
+                } else if currentLight == .yellow {
+                    redLight.opacity(0.2)
+                    yellowLight.opacity(1)
+                    greenLight.opacity(0.2)
+                } else if currentLight == .green {
+                    redLight.opacity(0.2)
+                    yellowLight.opacity(0.2)
+                    greenLight.opacity(1)
+                } else {
+                    redLight.opacity(0.2)
+                    yellowLight.opacity(0.2)
+                    greenLight.opacity(0.2)
+                }
+                
+                Spacer(minLength: 300)
+                
+                Button(action: {
+                    nameButton = "NEXT"
+                    nextColor()
+                }) {
+                    Text("\(nameButton)")
+                        .fontWeight(.bold)
+                        .font(.title)
+                        .padding()
+                        .frame(width: 250)
+                        .background(Color.blue)
+                        .cornerRadius(25)
+                        .foregroundColor(.white)
+                        .overlay(RoundedRectangle(cornerRadius: 25)
+                                    .stroke(Color.white, lineWidth: 4)
+                        )
+                }
+                
+            }
+        }.background(Color.black)
+    }
+}
+
+var redLight: some View {
+    Circle()
+        .foregroundColor(.red)
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+}
+
+var yellowLight: some View {
+    Circle()
+        .foregroundColor(.yellow)
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+}
+
+var greenLight: some View {
+    Circle()
+        .foregroundColor(.green)
+        .overlay(Circle().stroke(Color.white, lineWidth: 4))
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
+        }
     }
 }
